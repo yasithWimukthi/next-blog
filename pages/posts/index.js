9a345c273b0f0c34fdd2215899ef1d5664660433
yasith wimukthi <it19966922@my.sliit.lk>
2021-11-25 11:55:01 +0530

    implement posts grid component

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,3 +1,33 @@
-function AllPostsPage(){}
+import AllPosts from "../../components/posts/all-posts";
+
+const DUMMY_POSTS = [
+    {
+        slug:'getting-started-with-nextjs',
+        title: 'Getting started with NextJS',
+        image : 'getting-started-nextjs.png',
+        excerpt : 'Next.js is an open-source development framework built on top of Node.js enabling React based web applications functionalities such as server-side rendering and generating static websites.',
+        date: '2021-09-25'
+    },
+    {
+        slug:'getting-started-with-nextjs2',
+        title: 'Getting started with NextJS',
+        image : 'getting-started-nextjs.png',
+        excerpt : 'Next.js is an open-source development framework built on top of Node.js enabling React based web applications functionalities such as server-side rendering and generating static websites.',
+        date: '2021-09-25'
+    },
+    {
+        slug:'getting-started-with-nextjs3',
+        title: 'Getting started with NextJS',
+        image : 'getting-started-nextjs.png',
+        excerpt : 'Next.js is an open-source development framework built on top of Node.js enabling React based web applications functionalities such as server-side rendering and generating static websites.',
+        date: '2021-09-25'
+    }
+]
+
+function AllPostsPage(){
+    return (
+        <AllPosts posts={DUMMY_POSTS}/>
+    )
+}
 
 export default AllPostsPage;
