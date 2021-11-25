0396d7f386c60b504cc722b66659de6eee3ba055
yasith wimukthi <it19966922@my.sliit.lk>
2021-11-25 11:39:00 +0530

    implement posts grid component

--- components/home-page/featured-posts.js
+++ components/home-page/featured-posts.js
@@ -1,9 +1,11 @@
 import classes from './featured-posts.module.css'
+import PostGrid from "../posts/posts-grid";
 
-function FeaturedPosts(){
+function FeaturedPosts({posts}){
     return (
         <section className={classes.latest}>
             <h2> Featured Posts</h2>
+            <PostGrid posts={posts}/>
         </section>
     )
 }
