9e8395a0e2aba1bd294507000325687d27cfb370
yasith wimukthi <it19966922@my.sliit.lk>
2021-11-28 22:38:26 +0530

    implement posts content component

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,33 +1,21 @@
 import AllPosts from "../../components/posts/all-posts";
+import {getAllPosts} from "../../lib/posts-util";
 
-const DUMMY_POSTS = [
-    {
-        slug:'getting-started-with-nextjs',
-        title: 'Getting started with NextJS',
-        image : 'getting-started-nextjs.png',
-        excerpt : 'Next.js is an open-source development framework built on top of Node.js enabling React based web applications functionalities such as server-side rendering and generating static websites.',
-        date: '2021-09-25'
-    },
-    {
-        slug:'getting-started-with-nextjs2',
-        title: 'Getting started with NextJS',
-        image : 'getting-started-nextjs.png',
-        excerpt : 'Next.js is an open-source development framework built on top of Node.js enabling React based web applications functionalities such as server-side rendering and generating static websites.',
-        date: '2021-09-25'
-    },
-    {
-        slug:'getting-started-with-nextjs3',
-        title: 'Getting started with NextJS',
-        image : 'getting-started-nextjs.png',
-        excerpt : 'Next.js is an open-source development framework built on top of Node.js enabling React based web applications functionalities such as server-side rendering and generating static websites.',
-        date: '2021-09-25'
-    }
-]
-
-function AllPostsPage(){
+function AllPostsPage({posts}){
     return (
-        <AllPosts posts={DUMMY_POSTS}/>
+        <AllPosts posts={posts}/>
     )
 }
+
+export function getStaticProps(){
+    const allPosts = getAllPosts()
+
+    return {
+        props : {
+            posts : allPosts
+        }
+    }
+}
+
 
 export default AllPostsPage;
---
+++ posts/getting-started-with-nextjs.md
@@ -0,0 +1,11 @@
+---
+title : 'Getting started with NextJS'
+date : '2021-09-25'
+image : 'getting-started-nextjs.png'
+excerpt : 'Next.js is an open-source development framework built on top of Node.js enabling React based web applications functionalities such as server-side rendering and generating static websites.'
+isFeatured : true
+---
+
+# Title 
+
+content
